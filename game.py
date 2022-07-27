5e5fa15086104bb085e403c63e2f3b46c8be77fb
Luciano Silva <93166470+LucianoSilva2020@users.noreply.github.com>
2022-07-27 14:56:27 -0300

    ..

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 from random import randint
+from time import sleep
 itens = ('Pedra', 'Papel', 'Tesoura')
 computador = randint(0, 2)
 print('''Suas opções:
@@ -6,6 +7,11 @@ print('''Suas opções:
 [ 1 ] Papel
 [ 2 ] Tesoura''')
 jogador = int(input('Qual é sua jogada?'))
+print('JO')
+sleep(1)
+print('KEN')
+sleep(1)
+print('PO!!!')
 print('-=' * 12)
 print(f'Computador jogou {itens[computador]}')
 print(f'Jogador jogou {itens[jogador]}')
